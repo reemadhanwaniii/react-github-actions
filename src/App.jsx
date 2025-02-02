ab50647dcd78d6901945e199383e26c8000ab99a
Reema Dhanwani <reemadhanwani1234@gmail.com>
2025-02-02 21:05:57 +0530

    initial setup and workflows

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+        <h1> Hello World</h1>
+        <div>Welcome to react application</div>
+      </div>
+  )
+}
+
+export default App
